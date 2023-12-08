18bbd1763daf30c83eddf77943923a5da0324e82
Nazarii Yablonskyi <nazarii.yablonskyi@gmail.com>
2023-12-07 18:27:13 -0800

    typeorm + contact module

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -15,9 +15,9 @@ export const databaseProviders = [
         type: 'mysql',
         host: RDS_HOSTNAME,
         port: parseInt(RDS_PORT),
-        username: RDS_DB_NAME,
-        password: RDS_USERNAME,
-        database: RDS_PASSWORD,
+        username: RDS_USERNAME,
+        password: RDS_PASSWORD,
+        database: RDS_DB_NAME,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true, // TODO must be disabled in prod (the real prod)
       });
