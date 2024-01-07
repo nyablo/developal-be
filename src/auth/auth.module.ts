331b679b1a7e307e700f549bf7edfa073e221cb8
Nazarii Yablonskyi <nazarii.yablonskyi@gmail.com>
2024-01-07 01:32:42 -0800

    firebase-jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { DatabaseModule } from 'src/database/database.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { authProviders } from './auth.providers';
-import { DatabaseModule } from 'src/database/database.module';
 import { LocalStrategy } from './local.strategy';
-import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
@@ -13,8 +13,13 @@ import { JwtStrategy } from './jwt.strategy';
     DatabaseModule,
     PassportModule,
     JwtModule.register({
-      secretOrKeyProvider: () => process.env.JWT_SECRET,
-      signOptions: { expiresIn: '24h' },
+      secretOrKeyProvider: () => process.env.FIREBASE_PUBLIC_KEY,
+      signOptions: {
+        algorithm: 'RS256',
+      },
+      verifyOptions: {
+        algorithms: ['RS256'],
+      },
     }),
   ],
   controllers: [AuthController],
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,18 +1,43 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'firebase-jwt') {
   constructor() {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
-    });
+    super(
+      {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.FIREBASE_PUBLIC_KEY,
+        algorithms: ['RS256'],
+        requestProperty: 'user',
+        passReqToCallback: true,
+      },
+      async (req, _, done) => {
+        const jwt = req.get('Authorization').replace('Bearer ', '');
+        try {
+          const user = await this.validate(jwt);
+          return done(null, user);
+        } catch (error) {
+          return done(error);
+        }
+      },
+    );
+
+    if (admin.apps.length === 0) {
+      admin.initializeApp({
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        }),
+      });
+    }
   }
 
   async validate(payload: any) {
-    return payload.user;
+    const user = await admin.auth().verifyIdToken(payload, true);
+    return user;
   }
 }
